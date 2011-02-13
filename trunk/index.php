22b50473862da802139ec16531ad277a70f61345
1987lsm@gmail.com <1987lsm@gmail.com@a1cfc4c2-2210-da7b-f0e9-7d731617598c>
2011-02-13 13:59:05 +0000

    키보드파이터 2월 12일 버전

---
+++ trunk/index.php
@@ -0,0 +1,43 @@
+﻿<?include_once("header.php");?>
+
+<!--메인 페이지-->
+<h2>SD건담 키보드파이터에 오신 것을 환영합니다.</h2>
+<p>
+ 이 사이트는 SD건담 캡슐파이터(<a href="http://gundam.netmarble.net" target="_blank">http://gundam.netmarble.net</a>)의 정보를 쉽게 찾아보기 위해 만들어졌습니다.<br/>
+ 무료계정을 사용 중이기 때문에 일일 허용 트래픽(100MB) 초과시 사이트 접속이 원활하지 않을 수 있습니다.<br/>
+ 건의 사항 및 오류 제보는 만든이의 마이룸이나 블로그 방명록에 올려주시면 감사하겠습니다.<br/>
+ 캡파 닉네임 '에그'를 공방에서 만났을 때 아는 척 해주시면 무척 반갑겠습니다 =_=ㅋㅋ<br/>
+ <br/>
+ ps. 한주마 내려가고 나서도 트래픽 초과가 나는지 여부를 봐서 유료호스팅 전환을 고려중입니다 =_=;;;<br/>
+</p>	
+
+<h3>주요 업데이트 일자</h3>
+<p>
+<div id="update">
+2011.02.12 : 유닛 정보 메뉴에 검색 자료 항목 추가<br/>
+2011.02.04 : 레벨별 경험치 구간 테이블 추가<br/>
+2011.01.30 : 01월 20일 업데이트 반영. 내부 DB구조 변경. 유닛 목록 메뉴 개선. 스킬/무기 목록 메뉴 신설<br/>
+2010.12.23 : 12월 23일 업데이트 반영. 공홈에 오타가 왜이리 많어 OTL<br/>
+2010.11.18 : 11월 18일 업데이트 반영. 오버커 잘 되는 사람들은 치트키라도 쓴게 분명해...<br/>
+2010.10.21 : 10월 21일 업데이트 반영. 시험기간이라 기능추가는 없습니다 OTL<br/>
+2010.09.23 : 자료 테이블 메뉴 신설. 커스텀 초기화 비용 테이블 추가<br/>
+2010.09.17 : 유닛 정보, 캡슐 목록 약간 수정. 9월 16일 업데이트 반영<br/>
+2010.08.22 : 다른 페이지들도 자동완성기능 적용. 8월 19일 업데이트 반영<br/>
+2010.08.08 : 유닛 정보 페이지에 유닛명 자동완성기능 베타테스트 중<br/>
+2010.08.01 : 유저 계급표 페이지 신설. 7월 29일 업데이트 반영<br/>
+2010.07.25 : 유닛 정보 페이지 신설. 디자인 변경. 캡슐머신 검색오류 수정<br/>
+</div>
+</p>
+
+<h3>도움 주신 분들</h3>
+<p>
+<img src="http://img.gundam.netmarble.net/img/ranking/rank/10.gif" style="vertical-align:middle"/>&nbsp;<a href="javascript:GoMyroom('87de59d02fe185f15f9f708f9197e87c')">제네식엑시아</a>&nbsp;&nbsp;
+<img src="http://img.gundam.netmarble.net/img/ranking/rank/12.gif" style="vertical-align:middle"/>&nbsp;<a href="javascript:GoMyroom('10927f808b47ccf3dc7cf2a0e2167ddf')">참두부</a>&nbsp;&nbsp;
+<img src="http://img.gundam.netmarble.net/img/ranking/rank/10.gif" style="vertical-align:middle"/>&nbsp;<a href="javascript:GoMyroom('4f7348aa74886d4ba57f1481f395fa0c')">춤추는리가지</a>&nbsp;&nbsp;
+<img src="http://img.gundam.netmarble.net/img/ranking/rank/11.gif" style="vertical-align:middle"/>&nbsp;<a href="javascript:GoMyroom('33b22ceda218c5a6cfe6b4d42cc713b7')">메가날백</a>&nbsp;&nbsp;
+</p>
+
+<h3>만든이 정보</h3>
+<p><img src="http://img.gundam.netmarble.net/img/ranking/rank/9.gif" style="vertical-align:middle"/>&nbsp;<a href="javascript:GoMyroom('7971341297bc0e88fa4d3d02062499d5')">에그</a> (블로그: <a href="http://lsm1987.tistory.com" target="_blank">http://lsm1987.tistory.com</a>)</p>
+
+<?include_once("footer.php");?>
